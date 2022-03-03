071be83b9c831f75cd9f21aa036e857c09c25a82
Ade Mawan <ademawan1210@gmail.com>
2022-03-03 12:56:05 +0700

    entities

--- entities/user.go
+++ entities/user.go
@@ -6,8 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string `gorm:"type:varchar(100)" json:"name"`
-	Email    string `gorm:"unique" json:"email"`
-	Password string `json:"-"`
-	Gender   string `gorm:"type:enum('Pria','Wanita','None');default:'None'"`
+	User_uid string `gorm:"index;type:varchar(22)"`
+	Name     string `gorm:"type:varchar(100)"`
+	Email    string `gorm:"unique"`
+	Password string
+	Gender   string       `gorm:"type:enum('Pria','Wanita','None');default:'None'"`
+	Goal     Goal         `gorm:"foreignKey:User_uid;references:User_uid"`
+	History  User_history `gorm:"foreignKey:User_uid;references:User_uid"`
 }
