3c572a6fb76b5ea268b8b97d9b18a683f2d7c34f
aaryadewangga <arya.angga45@gmail.com>
2022-03-03 18:00:05 -0800

    user history open-api

--- entities/menu.go
+++ entities/menu.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Menu struct {
 	gorm.Model
 	Menu_uid       string `gorm:"index;type:varchar(22)"`
-	Menu_category  string `gorm:"type:enum('fruit','makan pagi','makan siang','makan malam');default:'None'"`
+	Menu_category  string `gorm:"type:enum('breakfast','lunch','dinner','None');default:'None'"`
 	Total_calories int
 	Foods          []Detail_menu `gorm:"foreignKey:Menu_uid;references:Menu_uid"`
 }
