c2fdd9719eed17dbaa2eab15fff8a31bdd66913d
aaryadewangga <arya.angga45@gmail.com>
2022-03-03 16:03:38 -0800

    update foods

--- entities/food.go
+++ entities/food.go
@@ -6,7 +6,7 @@ type Food struct {
 	gorm.Model
 	Food_uid      string `gorm:"index;type:varchar(22)"`
 	Name          string `gorm:"type:varchar(100)"`
-	Kalori        int    `gorm:"type:int(100)"`
-	Food_category string `gorm:"type:enum('fruit','minuman','junk food','food', 'snack');default:'None'"`
+	Calories      int    `gorm:"type:int(100)"`
+	Food_category string `gorm:"type:enum('fruit','drink','junk food','food', 'snack');default:'None'"`
 	Images        Image  `gorm:"foreignKey:Food_uid_uid;references:Food_uid"`
 }
--- entities/menu.go
+++ entities/menu.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Menu struct {
 	gorm.Model
-	Menu_uid      string `gorm:"index;type:varchar(22)"`
-	Menu_category string `gorm:"type:enum('fruit','makan pagi','makan siang','makan malam');default:'None'"`
-	Total_kalori  int
-	Foods         []Detail_menu `gorm:"foreignKey:Menu_uid;references:Menu_uid"`
+	Menu_uid       string `gorm:"index;type:varchar(22)"`
+	Menu_category  string `gorm:"type:enum('fruit','makan pagi','makan siang','makan malam');default:'None'"`
+	Total_calories int
+	Foods          []Detail_menu `gorm:"foreignKey:Menu_uid;references:Menu_uid"`
 }
