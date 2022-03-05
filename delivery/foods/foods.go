b46533b9248b756e72d833bf0501d026dd43b534
aaryadewangga <arya.angga45@gmail.com>
2022-03-05 04:19:02 -0800

    foods

---
+++ delivery/foods/foods.go
@@ -0,0 +1,151 @@
+package foods
+
+import (
+	"HealthFit/delivery/controllers/common"
+	"HealthFit/delivery/middlewares"
+	"HealthFit/entities"
+	"HealthFit/repository/foods"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type FoodsController struct {
+	repo foods.Foods
+}
+
+func New(repository foods.Foods) *FoodsController {
+	return &FoodsController{
+		repo: repository,
+	}
+}
+
+func (fc *FoodsController) Create() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		isAdmin := middlewares.ExtractRoles(c) // jangan lupa ganti extract token admin
+
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.BadRequest(http.StatusUnauthorized, "access denied", nil))
+		}
+
+		newFoods := FoodsCreateRequestFormat{}
+
+		c.Bind(&newFoods)
+		err := c.Validate(&newFoods)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest(http.StatusBadRequest, "There is some problem from input", nil))
+		}
+
+		res, err := fc.repo.Create(entities.Foods{
+			Name:          newFoods.Name,
+			Calories:      newFoods.Calories,
+			Energy:        newFoods.Energy,
+			Carbohidrate:  newFoods.Carbohidrate,
+			Protein:       newFoods.Protein,
+			Food_category: newFoods.Food_category,
+		})
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		response := FoodsCreateResponse{}
+		response.Food_uid = res.Food_uid
+		response.Name = res.Name
+		response.Calories = res.Calories
+		response.Energy = res.Energy
+		response.Carbohidrate = res.Carbohidrate
+		response.Protein = res.Protein
+		response.Food_category = res.Food_category
+
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "Success create foods", response))
+
+	}
+}
+
+func (fc *FoodsController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		isAdmin := middlewares.ExtractRoles(c) // jangan lupa ganti extract token admin
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.BadRequest(http.StatusUnauthorized, "access denied", nil))
+		}
+
+		food_uid := c.Param("food_uid")
+		var updateFoods = FoodsUpdateRequestFormat{}
+		c.Bind(&updateFoods)
+
+		err := c.Validate(&updateFoods)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest(http.StatusBadRequest, "There is some problem from input", nil))
+		}
+
+		res, err := fc.repo.Update(food_uid, entities.Foods{
+			Name:          updateFoods.Name,
+			Calories:      updateFoods.Calories,
+			Energy:        updateFoods.Energy,
+			Carbohidrate:  updateFoods.Carbohidrate,
+			Protein:       updateFoods.Protein,
+			Food_category: updateFoods.Food_category,
+		})
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		response := FoodsCreateResponse{}
+		response.Food_uid = res.Food_uid
+		response.Name = res.Name
+		response.Calories = res.Calories
+		response.Energy = res.Energy
+		response.Carbohidrate = res.Carbohidrate
+		response.Protein = res.Protein
+		response.Food_category = res.Food_category
+
+		return c.JSON(http.StatusCreated, common.Success(http.StatusCreated, "Success update foods", response))
+	}
+}
+
+func (fc *FoodsController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		isAdmin := middlewares.ExtractRoles(c) // jangan lupa ganti extract token admin
+		if !isAdmin {
+			return c.JSON(http.StatusUnauthorized, common.BadRequest(http.StatusUnauthorized, "access denied", nil))
+		}
+
+		food_uid := c.Param("food_uid")
+		err := fc.repo.Delete(food_uid)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "success delete foods", err))
+	}
+}
+
+func (fc *FoodsController) GetAll() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		res, err := fc.repo.GetAll()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		response := []FoodsGetAllResponse{}
+		for _, result := range res {
+			response = append(response, FoodsGetAllResponse{
+				Food_uid:      result.Food_uid,
+				Name:          result.Name,
+				Calories:      result.Calories,
+				Energy:        result.Energy,
+				Carbohidrate:  result.Carbohidrate,
+				Protein:       result.Protein,
+				Food_category: result.Food_category,
+				// Images: result.Images,
+			})
+		}
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "Success get all foods", response))
+	}
+}
