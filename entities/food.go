861d097eba2834b43a898010ecec3134b901d71e
Ade Mawan <ademawan1210@gmail.com>
2022-03-07 01:05:31 +0700

    queryresolved

--- entities/food.go
+++ entities/food.go
@@ -12,5 +12,5 @@ type Food struct {
 	Carbohidrate  int    `gorm:"type:int(100)"`
 	Protein       int    `gorm:"type:int(100)"`
 	Food_category string `gorm:"type:enum('fruit','drink','junk food','food','snack','None');default:'None'"`
-	Image         Image  `gorm:"foreignKey:Food_uid_uid;references:Food_uid"`
+	Image         Image  `gorm:"foreignKey:Food_uid;references:Food_uid"`
 }
