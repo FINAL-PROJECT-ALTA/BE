29c66cd77ad085eca7014a7bcd6d6023844e5576
aaryadewangga <arya.angga45@gmail.com>
2022-03-10 10:44:25 -0800

    update detailMenu

--- delivery/controllers/detail_menu/detailMenu.go
+++ delivery/controllers/detail_menu/detailMenu.go
@@ -65,3 +65,68 @@ func (dc *DetailMenuController) GetByUid() echo.HandlerFunc {
 		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "Succes to Get detail menu by id", response))
 	}
 }
+
+func (dc *DetailMenuController) GetAll() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		res, err := dc.repo.GetAllMenu()
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		response := []DetailMenuGetResponse{}
+		for _, result := range res {
+			response = append(response, DetailMenuGetResponse{
+				Detail_menu_uid: result.Detail_menu_uid,
+				Menu_uid:        result.Menu_uid,
+				Food_uid:        result.Food_uid,
+			})
+		}
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "Succes to Get detail menu by id", response))
+	}
+}
+
+func (dc *DetailMenuController) Update() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		detail_menu_uid := c.Param("detail_menu_uid")
+		var updateDetailMenu = DetailMenuCreateRequestFormat{}
+
+		c.Bind(&updateDetailMenu)
+
+		errB := c.Validate(&updateDetailMenu)
+		if errB != nil {
+			return c.JSON(http.StatusBadRequest, common.BadRequest(http.StatusBadRequest, "There is some problem from input", nil))
+		}
+		res, err := dc.repo.Update(detail_menu_uid, entities.Detail_menu{
+			Menu_uid: updateDetailMenu.Menu_uid,
+			Food_uid: updateDetailMenu.Food_uid,
+		})
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		response := DetailMenuCreateResponse{}
+		response.Detail_menu_uid = res.Detail_menu_uid
+		response.Menu_uid = res.Menu_uid
+		response.Food_uid = res.Food_uid
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "Succes to Get detail menu by id", response))
+	}
+}
+
+func (dc *DetailMenuController) Delete() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		detail_menu_uid := c.Param("detail_menu_uid")
+		err := dc.repo.Delete(detail_menu_uid)
+
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, common.InternalServerError(http.StatusInternalServerError, "There is some error on server", nil))
+		}
+
+		return c.JSON(http.StatusOK, common.Success(http.StatusOK, "success delete foods", nil))
+	}
+}
--- repository/detail_menu/interface.go
+++ repository/detail_menu/interface.go
@@ -6,6 +6,6 @@ type Detail_menu interface {
 	Create(newDetailMenu entities.Detail_menu) (entities.Detail_menu, error)
 	GetDetailMenuByUid(detail_menu_uid string) (entities.Detail_menu, error)
 	GetAllMenu() ([]entities.Detail_menu, error)
-	Update(detail_menu_uid string, updateDetailMenu entities.Detail_menu)
+	Update(detail_menu_uid string, updateDetailMenu entities.Detail_menu) (entities.Detail_menu, error)
 	Delete(detail_menu_uid string) error
 }
