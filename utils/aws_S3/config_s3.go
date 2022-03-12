3589d509c96b4ba10fb3c18c4cc0f0fd26509f8b
aaryadewangga <arya.angga45@gmail.com>
2022-03-13 03:44:37 +0700

    update enum

--- utils/aws_S3/config_s3.go
+++ utils/aws_S3/config_s3.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/labstack/gommon/log"
 	"github.com/lithammer/shortuuid"
@@ -65,4 +66,35 @@ func Upload(sess *session.Session, file multipart.File, fileHeader multipart.Fil
 	var url = "https://airbnb-app.s3.ap-southeast-1.amazonaws.com/" + uid
 
 	return url, nil
+}
+
+func UpdateUpload(sess *session.Session, filename string, file multipart.File, fileHeader multipart.FileHeader) string {
+
+	var src, err0 = fileHeader.Open()
+	if err0 != nil {
+		log.Info(err0)
+	}
+	defer src.Close()
+
+	size := fileHeader.Size
+	buffer := make([]byte, size)
+	src.Read(buffer)
+
+	var svc = s3.New(sess)
+
+	var input = &s3.PutObjectInput{
+		Bucket:       aws.String("airbnb-app"),
+		Key:          aws.String(filename),
+		ACL:          aws.String("public-read-write"),
+		Body:         bytes.NewReader(buffer),
+		ContentType:  aws.String(http.DetectContentType(buffer)),
+		StorageClass: aws.String("STANDARD"),
+	}
+
+	var res, err = svc.PutObject(input)
+	if err != nil {
+		log.Info(res)
+	}
+
+	return "success to update image"
 }
