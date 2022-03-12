9034ce0142d48c337cdce1abd54a4f89c669b6c7
aaryadewangga <arya.angga45@gmail.com>
2022-03-11 19:56:46 -0800

    userHistory

--- entities/user_history.go
+++ entities/user_history.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User_history struct {
 	gorm.Model
-	User_uid string
-	Menu_uid string
+	User_history_uid string
+	User_uid         string
+	Menu_uid         string
 }
--- repository/user_history/user_history.go
+++ repository/user_history/user_history.go
@@ -4,6 +4,7 @@ import (
 	"HealthFit/entities"
 	"errors"
 
+	"github.com/lithammer/shortuuid"
 	"gorm.io/gorm"
 )
 
@@ -17,11 +18,58 @@ func New(db *gorm.DB) *UserHistoryRepository {
 	}
 }
 
-func (uh *UserHistoryRepository) Register(newHistory entities.User_history) (entities.User_history, error) {
+func (uh *UserHistoryRepository) Insert(newHistory entities.User_history) (entities.User_history, error) {
+
+	uid := shortuuid.New()
+	newHistory.User_history_uid = uid
 
 	if err := uh.database.Create(&newHistory).Error; err != nil {
-		return newHistory, errors.New("invalid input")
+		return newHistory, err
+	}
+	return newHistory, nil
+}
+
+func (uh *UserHistoryRepository) GetAll(user_uid string) ([]entities.User_history, error) {
+	userHistory := []entities.User_history{}
+
+	if err := uh.database.Where("user_uid", user_uid).Find(&userHistory).Error; err != nil {
+		return userHistory, err
 	}
 
-	return newHistory, nil
+	return userHistory, nil
+}
+
+func (uh *UserHistoryRepository) GetById(user_history_uid string) (entities.User_history, error) {
+	user_history := entities.User_history{}
+
+	result := uh.database.Where("user_history_uid = ?", user_history_uid).First(&user_history)
+	if err := result.Error; err != nil {
+		return user_history, errors.New("record not found")
+	}
+
+	return user_history, nil
+}
+
+func (uh *UserHistoryRepository) Update(user_history_uid string, updateHistory entities.User_history) (entities.User_history, error) {
+	userHistory := entities.User_history{}
+
+	uh.database.Where("user_history_uid = ?", user_history_uid).First(&userHistory)
+
+	if err := uh.database.Model(&userHistory).Updates(&updateHistory).Error; err != nil {
+		return updateHistory, err
+	}
+
+	return updateHistory, nil
+}
+
+func (uh *UserHistoryRepository) Delete(user_history_uid string) error {
+	userHistory := entities.User_history{}
+
+	uh.database.Where("user_history_uid = ?", user_history_uid).First(&userHistory)
+
+	if err := uh.database.Delete(&userHistory).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
