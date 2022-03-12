5f2583246d7007026ebceb2dc0701e83d1483887
Ade Mawan <ademawan1210@gmail.com>
2022-03-13 06:50:08 +0700

    modify response json user

--- entities/user.go
+++ entities/user.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	gorm.Model
-	User_uid string `gorm:"index;unique;type:varchar(22)"`
-	Name     string `gorm:"type:varchar(100)"`
-	Email    string `gorm:"unique"`
-	Password string
-	Gender   string `gorm:"type:enum('male','female','none');default:'none'"`
-	Roles    bool   `gorm:"type:bool" json:"roles"`
-	Image    string
-	Goal     []Goal         `gorm:"foreignKey:User_uid;references:User_uid"`
-	History  []User_history `gorm:"foreignKey:User_uid;references:User_uid"`
+	gorm.Model `json:"-"`
+	User_uid   string         `gorm:"index;unique;type:varchar(22)" json:"user_uid"`
+	Name       string         `gorm:"type:varchar(100)" json:"name"`
+	Email      string         `gorm:"unique" json:"email"`
+	Password   string         `json:"-"`
+	Gender     string         `gorm:"type:enum('male','female','none');default:'none'" json:"gender"`
+	Roles      bool           `gorm:"type:bool" json:"roles"`
+	Image      string         `json:"image"`
+	Goal       []Goal         `gorm:"foreignKey:User_uid;references:User_uid" json:"goal"`
+	History    []User_history `gorm:"foreignKey:User_uid;references:User_uid" json:"history"`
 }
