c5c3263a6026235d46e5da5bd0aff944889ae079
Ade Mawan <ademawan1210@gmail.com>
2022-03-13 07:01:18 +0700

    modify response json goal

--- entities/goal.go
+++ entities/goal.go
@@ -1,17 +1,26 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Goal struct {
-	gorm.Model
-	Goal_uid      string `gorm:"index;type:varchar(22)"`
-	User_uid      string `gorm:"index;type:varchar(22)"`
-	Height        int
-	Weight        int
-	Age           int
-	Daily_active  string `gorm:"type:enum('not active','little active','quite active','active','very active')"`
-	Weight_target int
-	Range_time    int
-	Target        string `gorm:"type:enum('gain weight','lose weight')"`
-	Status        string `gorm:"type:enum('active', 'not active','cencel')"`
+	ID uint `gorm:"primarykey" json:"-"`
+
+	Goal_uid      string `gorm:"index;type:varchar(22)" json:"goal_uid"`
+	User_uid      string `gorm:"index;type:varchar(22)" json:"-"`
+	Height        int    `json:"height"`
+	Weight        int    `json:"weight"`
+	Age           int    `json:"age"`
+	Daily_active  string `gorm:"type:enum('not active','little active','quite active','active','very active')" json:"daily_active"`
+	Weight_target int    `json:"weight_target"`
+	Range_time    int    `json:"range_time"`
+	Target        string `gorm:"type:enum('gain weight','lose weight')" json:"target"`
+	Status        string `gorm:"type:enum('active', 'not active','cencel')" json:"status"`
+
+	CreatedAt time.Time      `json:"cretedAt"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
