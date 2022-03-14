3d5df33721d8dc003f74d5c10bd21133cd47cd01
Ade Mawan <ademawan1210@gmail.com>
2022-03-15 04:09:13 +0700

    add counting target gainWeight GetRecommendDinner

--- repository/menu/menu.go
+++ repository/menu/menu.go
@@ -303,9 +303,18 @@ func (mr *MenuRepository) GetRecommendDinner(user_uid string) ([]entities.Menu, 
 
 	menus := []entities.Menu{}
 
-	start := dinner * 50 / 100
+	var start int
+	var end int
+	if target == "gain weight" {
+		start = dinner
+		rangeGain := dinner * 50 / 100
+		end = dinner + rangeGain
+	} else if target == "lose weight" {
+		start = dinner * 50 / 100
+		end = dinner
+	}
 
-	res := mr.database.Debug().Preload("Detail_menu").Preload("Detail_menu.Food").Where("menu_category=? AND created_by = ? AND total_calories BETWEEN ? AND ?", "dinner", "admin", start, dinner).Order("count desc").Find(&menus)
+	res := mr.database.Debug().Preload("Detail_menu").Preload("Detail_menu.Food").Where("menu_category=? AND created_by = ? AND total_calories BETWEEN ? AND ?", "dinner", "admin", start, end).Order("count desc").Find(&menus)
 
 	if err := res.Error; err != nil {
 		return menus, 0, 0, err
