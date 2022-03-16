d24b7748732b701f8fb528e06543470d760092b3
Ade Mawan <ademawan1210@gmail.com>
2022-03-16 20:43:51 +0700

    solve response get ById2

--- repository/user_history/user_history.go
+++ repository/user_history/user_history.go
@@ -7,7 +7,6 @@ import (
 	"github.com/labstack/gommon/log"
 	"github.com/lithammer/shortuuid"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type UserHistoryRepository struct {
@@ -63,7 +62,7 @@ func (uh *UserHistoryRepository) GetAll(user_uid string) ([]entities.User_histor
 func (uh *UserHistoryRepository) GetById(user_uid, user_history_uid string) (entities.User_history, error) {
 	user_history := entities.User_history{}
 
-	result := uh.database.Preload(clause.Associations).Where("user_uid = ? AND user_history_uid = ?", user_uid, user_history_uid).First(&user_history)
+	result := uh.database.Preload("Menu").Preload("Detail_menu.Food").Where("user_uid = ? AND user_history_uid = ?", user_uid, user_history_uid).First(&user_history)
 	if err := result.Error; err != nil {
 		return user_history, errors.New("record not found")
 	}
