301667382a5f4e360c694c6f059bbd3397cba13f
aaryadewangga <arya.angga45@gmail.com>
2022-03-16 12:42:46 +0700

    update test repo food

--- entities/detail_menu.go
+++ entities/detail_menu.go
@@ -12,7 +12,7 @@ type Detail_menu struct {
 	Detail_menu_uid string         `json:"-"`
 	Menu_uid        string         `gorm:"index;type:varchar(22)" json:"-"`
 	Food_uid        string         `gorm:"index;type:varchar(22)" json:"-"`
-	Food            Food           `gorm:"foreignKey:Food_uid;references:Food_uid" json:"food"`
+	Food            Food           `gorm:"-" json:"food"`
 	CreatedAt       time.Time      `json:"-"`
 	UpdatedAt       time.Time      `json:"-"`
 	DeletedAt       gorm.DeletedAt `gorm:"index" json:"-"`
