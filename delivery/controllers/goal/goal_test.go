788fd984e14af804ec0351f8a4a9908fab6e278e
Ade Mawan <ademawan1210@gmail.com>
2022-03-17 23:51:49 +0700

    F failed access create goal test

--- delivery/controllers/goal/goal_test.go
+++ delivery/controllers/goal/goal_test.go
@@ -243,6 +243,47 @@ func TestCreate(t *testing.T) {
 		assert.Equal(t, "There is some problem from input", resp.Message)
 
 	})
+
+	t.Run("Failed access create", func(t *testing.T) {
+		e := echo.New()
+		e.Validator = &CustomValidator{validator: validator.New()}
+
+		reqBody, _ := json.Marshal(map[string]interface{}{
+			"height":        150,
+			"weight":        55,
+			"age":           24,
+			"daily_active":  "not active",
+			"weight_target": 2,
+			"range_time":    30,
+			"target":        "lose weight",
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		req.Header.Set("Content-Type", "application/json")
+
+		context := e.NewContext(req, res)
+		context.SetPath("/users/goals")
+
+		goalController := New(&MockFailedGoalRepository{})
+
+		err := middleware.JWT([]byte(configs.JWT_SECRET))(goalController.Create())(context)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var resp common.Response
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &resp)
+		assert.Equal(t, float64(http.StatusBadRequest), resp.Code)
+		assert.Equal(t, "access denied ", resp.Message)
+
+	})
 }
 
 type MockGoalRepository struct{}
