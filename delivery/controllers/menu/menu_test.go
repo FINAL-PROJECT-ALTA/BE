8ced4d2b99405bb1f475755365df24ba6a97e72d
Ade Mawan <ademawan1210@gmail.com>
2022-03-18 15:38:44 +0700

    F Create failed bind byAdmin menu test

--- delivery/controllers/menu/menu_test.go
+++ delivery/controllers/menu/menu_test.go
@@ -311,6 +311,44 @@ func TestCreate(t *testing.T) {
 		assert.Equal(t, "There is some problem from input", resp.Message)
 
 	})
+
+	t.Run("Failed bind create by admin", func(t *testing.T) {
+		e := echo.New()
+		e.Validator = &CustomValidator{validator: validator.New()}
+
+		reqBody, _ := json.Marshal(map[string]interface{}{
+			"menu_category": "",
+			"foods":         []entities.Food{},
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		req.Header.Set("Content-Type", "application/json")
+
+		context := e.NewContext(req, res)
+		context.SetPath("/menus")
+
+		goalController := New(&MockFailedMenuRepository{})
+
+		// goalController.Create()(context)
+
+		err := middleware.JWT([]byte(configs.JWT_SECRET))(goalController.Create())(context)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var resp common.Response
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &resp)
+		assert.Equal(t, float64(http.StatusBadRequest), resp.Code)
+		assert.Equal(t, "There is some problem from input", resp.Message)
+
+	})
 }
 
 type MockMenuRepository struct{}
