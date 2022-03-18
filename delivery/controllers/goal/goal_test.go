080c176065a7002a81073f67ddf2a0f0b6b56ce9
Ade Mawan <ademawan1210@gmail.com>
2022-03-18 14:50:41 +0700

    F Delete access goal test

--- delivery/controllers/goal/goal_test.go
+++ delivery/controllers/goal/goal_test.go
@@ -683,6 +683,38 @@ func TestDelete(t *testing.T) {
 		assert.Equal(t, "There is some error on server", resp.Message)
 
 	})
+	t.Run("Failed access delete", func(t *testing.T) {
+		e := echo.New()
+		e.Validator = &CustomValidator{validator: validator.New()}
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenAdmin))
+
+		req.Header.Set("Content-Type", "application/json")
+
+		context := e.NewContext(req, res)
+		context.SetPath("/users/goals")
+		context.SetParamNames("goal_uid")
+		context.SetParamValues("xyz")
+
+		goalController := New(&MockFailedGoalRepository{})
+
+		err := middleware.JWT([]byte(configs.JWT_SECRET))(goalController.Delete())(context)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var resp common.Response
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &resp)
+		assert.Equal(t, float64(http.StatusBadRequest), resp.Code)
+		assert.Equal(t, "access denied ", resp.Message)
+
+	})
+
 }
 
 type MockGoalRepository struct{}
