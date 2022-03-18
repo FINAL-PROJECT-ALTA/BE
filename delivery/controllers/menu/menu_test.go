b8255ee319ec10943f989fe97bb99389fc3f224c
Ade Mawan <ademawan1210@gmail.com>
2022-03-18 15:36:20 +0700

    F Create failed byUser menu test

--- delivery/controllers/menu/menu_test.go
+++ delivery/controllers/menu/menu_test.go
@@ -197,6 +197,44 @@ func TestCreate(t *testing.T) {
 		assert.Equal(t, "Success create menu", resp.Message)
 
 	})
+
+	t.Run("Failed create by user", func(t *testing.T) {
+		e := echo.New()
+		e.Validator = &CustomValidator{validator: validator.New()}
+
+		reqBody, _ := json.Marshal(map[string]interface{}{
+			"menu_category": "food",
+			"foods":         []entities.Food{},
+		})
+
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", jwtTokenUser))
+
+		req.Header.Set("Content-Type", "application/json")
+
+		context := e.NewContext(req, res)
+		context.SetPath("/menus")
+
+		goalController := New(&MockFailedMenuRepository{})
+
+		// goalController.Create()(context)
+
+		err := middleware.JWT([]byte(configs.JWT_SECRET))(goalController.Create())(context)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var resp common.Response
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &resp)
+		assert.Equal(t, float64(http.StatusInternalServerError), resp.Code)
+		assert.Equal(t, "There is some error on server", resp.Message)
+
+	})
 }
 
 type MockMenuRepository struct{}
