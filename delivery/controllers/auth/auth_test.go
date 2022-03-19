5c2fd488af24958b3422d8aeaee7890d9e17f1c5
Ade Mawan <ademawan1210@gmail.com>
2022-03-19 21:37:20 +0700

    F Login failed bind auth test

--- delivery/controllers/auth/auth_test.go
+++ delivery/controllers/auth/auth_test.go
@@ -151,6 +151,35 @@ func TestLogin(t *testing.T) {
 			assert.Equal(t, float64(http.StatusUnauthorized), response.Code)
 		},
 	)
+
+	t.Run(
+		"1. failed bind Login User Test", func(t *testing.T) {
+			e := echo.New()
+			e.Validator = &CustomValidator{validator: validator.New()}
+
+			requestBody, _ := json.Marshal(
+				LoginReqFormat{
+					Email:    "tetuser",
+					Password: "testuser",
+				},
+			)
+
+			req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+			res := httptest.NewRecorder()
+			req.Header.Set("Content-Type", "application/json")
+			context := e.NewContext(req, res)
+			context.SetPath("/users/login")
+
+			authController := New(&MockAuthIncorrectPasswordRepository{})
+			authController.Login()(context)
+
+			var response common.Response
+
+			json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+			assert.Equal(t, float64(http.StatusBadRequest), response.Code)
+		},
+	)
 }
 
 type MockAuthRepository struct{}
